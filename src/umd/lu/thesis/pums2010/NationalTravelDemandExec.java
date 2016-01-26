f68249cd6ef6cc0465c67f9979b7f9c4be249db4
Bo Sun <bosun58@gmail.com>
2016-01-25 22:46:00 -0500

    Refactor. Extends exec class from ThesisBase. Also add global try-catch to log stacktraces.

--- src/umd/lu/thesis/pums2010/NationalTravelDemandExec.java
+++ src/umd/lu/thesis/pums2010/NationalTravelDemandExec.java
@@ -4,6 +4,8 @@
  */
 package umd.lu.thesis.pums2010;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import umd.lu.thesis.common.ThesisBase;
 
 /**
@@ -11,13 +13,20 @@ import umd.lu.thesis.common.ThesisBase;
  * @author Home
  */
 public class NationalTravelDemandExec extends ThesisBase{
+    private final static Logger sLog = LogManager.getLogger(NationalTravelDemandExec.class);
     
     public static void main(String[] args) throws Exception {
         int start = Integer.parseInt(args[0]);
         int end = Integer.parseInt(args[1]);
 //        int start = 73030;
 //        int end = 73031;
-        NationalTravelDemand runner = new NationalTravelDemand();
-        runner.run(start, end);
+        try {
+            ThesisBase tb = new ThesisBase();
+            NationalTravelDemand runner = new NationalTravelDemand();
+            runner.run(start, end);
+        }
+        catch (Exception e) {
+            sLog.error("error: ", e);
+        }
     }
 }
